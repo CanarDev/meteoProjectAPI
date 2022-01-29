c26bbcee61c6ce33a557b8d3b37beb7f2fe55e24
CanarDev <vincent.michel@edu.devinci.Fr>
2022-01-29 19:32:57 +0100

    :sparkles: SearchBar ! when you go to the website, you see your meteo and you can search the weather in other city and other countries

--- js/main.js
+++ js/main.js
@@ -1,66 +1,137 @@
 //Recuperer l'addresse IP
-const options = {
-    method: 'post',
-    headers: {
-        Accept: 'application/json'
-    }
-};
+
+// keys
 const weatherKey = "07d06524f38e4601a78223627222301"
 const unsplashKey = "qhk0xx7XagH1GYxZtFDMnPYWRPKpkf59UaYdoxj-vkA"
-const body = document.querySelector('body')
-const degArroundMe = document.querySelector('#degArroundMe')
-const firstView = document.querySelector('.firstView')
+// keys
 
-// // etch('http://ipwhois.app/json/')   
-// //    .then(response => response.json())
-// //    .then(response => {
-// //        console.log(response)
-// //        document.cookie = response.ip
-// //        console.log(document.cookie)
-// // )  https://devinci-online.brightspace.com/d2l/common/rpc/dataList/dataList.d2lfile?ou=6606&d2l_rh=rpc&d2l_rt=call
-// 
+const body = document.querySelector('body')
+const firstView = document.querySelector('.firstView')
+const searchCity = document.querySelector('.searchCity')
+const searchButton = document.querySelector('.searchButton')
+
 fetch('https://ipwhois.app/json/')   
     .then(response => response.json())
     .then(response => {
         console.log(response)
-        fetch('https://api.weatherapi.com/v1/current.json?key=' + weatherKey + '&q=' + response.ip + '&aqi=no')
+        let address = response.ip
+        searchCity.addEventListener('keydown', (event) => event.key === 'Enter' && searchButton.click());
+        searchButton.addEventListener('click', () => {
+            localStorage.setItem('locate', searchCity.value)
+            //address = localStorage.getItem('locate'); 
+        });
+        var City = ""
+        if (localStorage.getItem('locate') === null){
+            City = address
+            console.log('')
+        }else{
+            City = localStorage.getItem('locate');
+        }
+
+        fetch('https://api.weatherapi.com/v1/current.json?key=' + weatherKey + '&q=' + City + '&aqi=no')
             .then(response => response.json())
             .then(data => {
                 console.log(data)
+                
+
+                const cityName = document.createElement('div')
+
+                const degArroundMe = document.createElement('div')
+
+                const degCity = document.createElement('h1')
+
+                const degCard = document.createElement('div')
                 const degLogo = document.createElement('img')
                 const degInfo = document.createElement('p')
-                const degCard = document.createElement('div')
-                const degCity = document.createElement('h1')
-                const cityName = document.createElement('div')
+
+                const buttonChange = document.createElement('div')
+                const textButton = document.createElement('p')
+                
+
+
+
                 cityName.classList.add('cityName')
                 const locationName = data.location.name
                 degCity.innerHTML = locationName
                 console.log(locationName)
+                cityName.appendChild(degCity)
+                firstView.appendChild(cityName)
+                
+
+
+                degArroundMe.id = 'degArroundMe'
                 const locationCountry = data.location.country
                 console.log(locationCountry)
                 degCard.classList.add('degCard')
                 degLogo.src = data.current.condition.icon
                 degInfo.innerHTML = data.current.temp_c +'℃'
-                cityName.appendChild(degCity)
-                firstView.appendChild(cityName)
                 degCard.appendChild(degLogo)
                 degCard.appendChild(degInfo)
                 degArroundMe.appendChild(degCard)
-                const x = Math.floor(Math.random() * 8)
+                firstView.appendChild(degArroundMe)
+
+
+
+                textButton.innerHTML = 'Click here to change the background !'
+                textButton.classList.add('textButton')
+                buttonChange.appendChild(textButton)
+                buttonChange.classList.add('changeButton')
+                firstView.appendChild(buttonChange)
+
+
                 fetch('https://api.unsplash.com/search/photos/?client_id=' + unsplashKey + '&page=1&per_page=5&orientation=landscape&query=' + locationName + ' ' + locationCountry)
+                
                     .then(response => response.json())
                     .then(dataUnsplash => {
+
+                        const n = Math.floor(Math.random() * dataUnsplash.results.length)
+
+                        const color = dataUnsplash.results[n].color
+
+                        searchCity.style.backgroundColor = color
+                        searchButton.style.backgroundColor = color
                         
                         console.log(dataUnsplash.results)
-                        const n = Math.floor(Math.random() * dataUnsplash.results.length)
                         console.log(n)
-                        const color = dataUnsplash.results[n].color
+                        
                         body.style.backgroundColor = color
                         degCard.style.backgroundColor = color
                         body.style.backgroundImage = 'url(' + dataUnsplash.results[n].urls.raw + ')' 
                         degInfo.style.color = color
                         degCity.style.color = color
                         cityName.style.backgroundColor = color
+                        textButton.style.color = color
+                        buttonChange.style.backgroundColor = color
+
+                        buttonChange.addEventListener('click', (event) => {
+                            fetch('https://api.unsplash.com/search/photos/?client_id=' + unsplashKey + '&page=1&per_page=5&orientation=landscape&query=' + locationName + ' ' + locationCountry)
+                            .then(response => response.json())
+                            .then(dataUnsplash => {
+                                
+                                const n = Math.floor(Math.random() * dataUnsplash.results.length)
+
+                                const color = dataUnsplash.results[n].color
+
+                                searchCity.style.backgroundColor = color
+                                searchButton.style.backgroundColor = color
+                                
+                                console.log(dataUnsplash.results)
+                                console.log(n)
+                                
+                                body.style.backgroundColor = color
+                                degCard.style.backgroundColor = color
+                                body.style.backgroundImage = 'url(' + dataUnsplash.results[n].urls.raw + ')' 
+                                degInfo.style.color = color
+                                degCity.style.color = color
+                                cityName.style.backgroundColor = color
+                                textButton.style.color = color
+                                buttonChange.style.backgroundColor = color
+
+
+                                })
+                        })
                     })
             })
     })
+
+
